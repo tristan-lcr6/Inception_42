17aebc15c3f6f21ca00281f879847e9b1583d2d3
tristan-lcr6 <tristan.l0244@gmail.com>
2026-05-07 16:54:33 +0200

    add passw ftp

--- srcs/requirements/bonus/FTP-server/ftp-setup.sh
+++ srcs/requirements/bonus/FTP-server/ftp-setup.sh
@@ -4,7 +4,8 @@ useradd -m -d /var/www/html/wordpress $FTP_USER
 
 chown -R $FTP_USER:$FTP_USER /var/www/html/wordpress
 
-# cat > /ect/vsftpd.conf << CONF
+echo "$FTP_USER:$FTP_PWD" | chpasswd
+
 listen=YES
 listen_ipv6=YES
 
