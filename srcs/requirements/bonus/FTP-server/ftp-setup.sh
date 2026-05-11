28b68c11b9994c68a9963711a4a2fd71581b829b
tristan-lcr6 <tristan.l0244@gmail.com>
2026-05-11 19:29:31 +0200

    change script in ftp.sh

--- srcs/requirements/bonus/FTP-server/ftp-setup.sh
+++ srcs/requirements/bonus/FTP-server/ftp-setup.sh
@@ -1,17 +1,45 @@
 #!/bin/bash
 
-useradd -m -d /var/www/html/wordpress $FTP_USER
+FTP_PASSWORD=$(cat /run/secrets/ftp_password)
 
-chown -R $FTP_USER:$FTP_USER /var/www/html/wordpress
+# Create required directory for vsftpd
+mkdir -p /var/run/vsftpd/empty
+chmod 755 /var/run/vsftpd/empty
 
-echo "$FTP_USER:$FTP_PWD" | chpasswd
+# Create FTP user
+useradd -m ftpuser
 
-listen=YES
-listen_ipv6=YES
+echo "ftpuser:${FTP_PASSWORD}" | chpasswd
 
-anonymous_enable=NO
-local_enable=YES
-write_enable=YES
-user_lst_enable=NO
+usermod -aG www-data ftpuser
 
+# Give access to WordPress files
+mkdir -p /var/www/html
+chown -R www-data:www-data /var/www/html
+chmod -R 775 /var/www/html
+
+# Start FTP
 exec vsftpd /etc/vsftpd.conf
+
+
+
+
+
+
+
+
+# useradd -m -d /var/www/html/wordpress $FTP_USER
+
+# chown -R $FTP_USER:$FTP_USER /var/www/html/wordpress
+
+# echo "$FTP_USER:$FTP_PWD" | chpasswd
+
+# listen=YES
+# listen_ipv6=YES
+
+# anonymous_enable=NO
+# local_enable=YES
+# write_enable=YES
+# user_lst_enable=NO
+
+# exec vsftpd /etc/vsftpd.conf
