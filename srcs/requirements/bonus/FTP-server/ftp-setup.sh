6cdc0fd3ea89aea9a13c93aa813e8bf121c94b69
tristan-lcr6 <tristan.l0244@gmail.com>
2026-05-12 12:51:39 +0200

    add setup

--- srcs/requirements/bonus/FTP-server/ftp-setup.sh
+++ srcs/requirements/bonus/FTP-server/ftp-setup.sh
@@ -9,13 +9,32 @@ chown -R $FTP_USER:$FTP_USER /var/www/html/worpress
 cat > etc/vsftpd.conf << EOF
 listen=YES
 listen_ipv6=NO
+
 anonymous_enable=NO
 local_enable=YES
+write_enable=YES
+
+local_umask=022
+
+dirmessage_enable=YES
+use_localtime=YES
+
+xferlog_enable=YES
+connect_from_port_20=YES
+
 chroot_local_user=YES
 allow_writeable_chroot=YES
+
 pasv_enable=YES
-pasv_min_port=21000
-pasv_max_port=21010
+pasv_min_port=30000
+pasv_max_port=30010
+
+user_sub_token=$USER
+local_root=/var/www/html
+
+secure_chroot_dir=/var/run/vsftpd/empty
+
+pam_service_name=vsftpd
 EOF
 
 mkdir -p /var/www/html
